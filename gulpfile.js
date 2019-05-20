b26883a8ff0df2c56f7014dac57757edf12cd712
tsubasa.masaki <tsubasa.masaki@tsubasamasakinoMacBook-puro.local>
2019-05-20 09:29:29 +0900

    lint imagemin 変更

--- gulpfile.js
+++ gulpfile.js
@@ -1,79 +1,88 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
+var gulp = require("gulp");
+var sass = require("gulp-sass");
 
-var plumber = require('gulp-plumber');
-var notify = require('gulp-notify');
-var sassGlob = require('gulp-sass-glob');
-var mmq = require('gulp-merge-media-queries');
-var browserSync = require('browser-sync');
+var plumber = require("gulp-plumber");
+var notify = require("gulp-notify");
+var sassGlob = require("gulp-sass-glob");
+var mmq = require("gulp-merge-media-queries");
+var browserSync = require("browser-sync");
 
-var imagemin = require('gulp-imagemin');
-var imageminPngquant = require('imagemin-pngquant');
-var imageminMozjpeg = require('imagemin-mozjpeg');
+var imagemin = require("gulp-imagemin");
+var imageminPngquant = require("imagemin-pngquant");
+var imageminMozjpeg = require("imagemin-mozjpeg");
 
-var postcss = require('gulp-postcss');
-var autoprefixer = require('autoprefixer');
+var postcss = require("gulp-postcss");
+var autoprefixer = require("autoprefixer");
 
-var cssdeclsort = require('css-declaration-sorter');
+var cssdeclsort = require("css-declaration-sorter");
 
 var imageminOption = [
-	imageminPngquant({ quality: '65-80' }),
-	imageminMozjpeg({ quality: 85 }),
-	imagemin.gifsicle({
-		interlaced: false,
-		optimizationLevel: 1,
-		colors: 256
-	}),
-	imagemin.jpegtran(),
-	imagemin.optipng(),
-	imagemin.svgo()
+  imageminPngquant({ quality: "65-80" }),
+  imageminMozjpeg({ quality: 85 }),
+  imagemin.gifsicle({
+    interlaced: false,
+    optimizationLevel: 1,
+    colors: 256
+  }),
+  imagemin.jpegtran(),
+  imagemin.optipng(),
+  imagemin.svgo()
 ];
 
-gulp.task('sass', function () {
-	return gulp
-		.src('./sass/**/*.scss')
-		.pipe(plumber({ errorHandler: notify.onError('Error: <%= error.message %>') }))
-		.pipe(sassGlob())
-		.pipe(sass({ outputStyle: 'expanded' }))
-		.pipe(postcss([autoprefixer({
-			browsers: ["last 2 versions", "ie >= 11", "Android >= 4"],
-			grid: true,
-			cascade: false
-		})]))
-		.pipe(postcss([cssdeclsort({ order: 'SMACSS' })]))
-		.pipe(mmq())
-		.pipe(gulp.dest('./css'));
+gulp.task("sass", function() {
+  return gulp
+    .src("./sass/**/*.scss")
+    .pipe(
+      plumber({ errorHandler: notify.onError("Error: <%= error.message %>") })
+    )
+    .pipe(sassGlob())
+    .pipe(sass({ outputStyle: "expanded" }))
+    .pipe(
+      postcss([
+        autoprefixer({
+          browsers: ["last 2 versions", "ie >= 11", "Android >= 4"],
+          grid: true,
+          cascade: false
+        })
+      ])
+    )
+    .pipe(postcss([cssdeclsort({ order: "SMACSS" })]))
+    .pipe(mmq())
+    .pipe(gulp.dest("./css"));
 });
 
-gulp.task('sass-watch', function () {
-	gulp.watch('./sass/**/*.scss', gulp.task('sass'));
+gulp.task("sass-watch", function() {
+  gulp.watch("./sass/**/*.scss", gulp.task("sass"));
 });
 
-gulp.task('browser-sync', function () {
-	browserSync.init({
-		server: {
-			baseDir: './',
-			index: 'index.html'
-		}
-	});
+gulp.task("browser-sync", function() {
+  browserSync.init({
+    server: {
+      baseDir: "./",
+      index: "index.html"
+    }
+  });
 });
 
-gulp.task('bs-reload', function (done) {
-	browserSync.reload();
-	done();
+gulp.task("bs-reload", function(done) {
+  browserSync.reload();
+  done();
 });
 
-gulp.task('file-watch', function () {
-	gulp.watch('**/*.html', gulp.task('bs-reload'));
-	gulp.watch('./css/*.css', gulp.task('bs-reload'));
-	gulp.watch('./js/*.js', gulp.task('bs-reload'));
+gulp.task("file-watch", function() {
+  gulp.watch("**/*.html", gulp.task("bs-reload"));
+  gulp.watch("./css/*.css", gulp.task("bs-reload"));
+  gulp.watch("./js/*.js", gulp.task("bs-reload"));
 });
 
-gulp.task("default", gulp.series(gulp.parallel("browser-sync", "file-watch", "sass-watch")));
+gulp.task(
+  "default",
+  gulp.series(gulp.parallel("browser-sync", "file-watch", "sass-watch"))
+);
 
-gulp.task('imagemin', function () {
-	return gulp
-		.src('./img/base/*.{png,jpg,gif,svg}')
-		.pipe(imagemin(imageminOption))
-		.pipe(gulp.dest('./img'));
+gulp.task("imagemin", function() {
+  return gulp
+    .src("./img/base/*.{png,jpg,gif,svg}")
+    .pipe(imagemin(imageminOption))
+    .pipe(gulp.dest("./img"));
 });
